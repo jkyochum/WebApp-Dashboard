d2f6a693d952c05c3e2d2d4c687907f74c445215
Jeremy Yochum <jyochum391@yahoo.com>
2023-05-22 21:49:03 -0400

    add charts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
 
-const ctx = document.getElementById('myChart');
+const lineCtx = document.getElementById('lineChart');
+const barCtx = document.getElementById('barChart');
+const donutCtx = document.getElementById('doughnutChart');
 
-new Chart(ctx, {
+new Chart(lineCtx, {
     type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(barCtx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(donutCtx, {
+    type: 'doughnut',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
