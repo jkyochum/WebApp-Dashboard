2702b7bb0b55fc5d436c5cdf9dfe0dedc111344a
Jeremy Yochum <jyochum391@yahoo.com>
2023-06-15 21:48:13 -0400

    create search script and link into html / create user array for searching

---
+++ js/search.js
@@ -0,0 +1,17 @@
+const users = [
+    'victoria chambers',
+    'dale byrd',
+    'dawn wood',
+    'dan oliver',
+    'jimmy garcia',
+    'winston bonavue',
+    'john wick',
+    'davy jones'
+];
+
+//filter search results based on input from user
+
+function search() {
+    let input = document.getElementById('searchUserText').value;
+
+}
