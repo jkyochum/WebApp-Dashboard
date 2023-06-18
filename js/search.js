a1b67dfa82240bd40068c5cefd05a3e6f52a10b4
Jeremy Yochum <jyochum391@yahoo.com>
2023-06-18 01:09:24 -0400

    create filtered search list / add click functionality to search list

--- js/search.js
+++ js/search.js
@@ -1,25 +1,49 @@
+const ul = document.querySelector('#searchResults ul');
+const results = document.getElementById('searchResults');
+const input = document.getElementById('searchUserText');
 const users = [
-    'victoria chambers',
-    'dale byrd',
-    'dawn wood',
-    'dan oliver',
-    'jimmy garcia',
-    'winston bonavue',
-    'john wick',
-    'davy jones'
+    'Victoria Chambers',
+    'Dale Byrd',
+    'Dawn Wood',
+    'Dan Oliver',
+    'Jimmy Garcia',
+    'Winston Bonavue',
+    'John Wick',
+    'Davy Jones'
 ];
 
 //filter search results based on input from user
 
 function search() {
+    const results = [];
+    ul.innerHTML = '';
+
     let input = document.getElementById('searchUserText').value;
     input = input.toLowerCase();
-    document.createElement('ul');
-    document.createElement('li');
 
     for (let i = 0; i < users.length; i++) {
+        if (users[i].toLowerCase().includes(input)) {
+            results.push(users[i]);
+        }
+        else {
+            ul.classList.remove('has-results');
+        }
+    }
 
+    // ul.innerHTML = '';
+
+    for (let i = 0; i < results.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = results[i];
+        ul.appendChild(li);
+        ul.classList.add('has-results');
     }
 
 
 }
+
+results.addEventListener('click', (e) => {
+    console.log(e);
+    input.value = e.target.innerText;
+    ul.classList.remove('has-results');
+})
